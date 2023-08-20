c3c74839002c8eccc1f530b73759aa608d0faa8d
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-20 22:12:27 +0100

    Added link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 interface NavbarProps { }
 export const Navbar = () => {
   return (
     <nav className='fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2'>
       <div className='flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl'>
-        <p className=' rounded-lg border-2 border-b-4 border-r4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>Learnify</p>
+        <Link href={"/gallery"} className='items-center hidden gap-2 sm:flex'>
+          <p className=' rounded-lg border-2 border-b-4 border-r4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>Learnify</p>
+        </Link>
       </div>
     </nav>
   );
