c595018dda0fe1f39a44aba735447f4c33d909b4
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 06:34:23 +0100

    Started working on default values for form

--- src/components/CreateCourseForm.tsx
+++ src/components/CreateCourseForm.tsx
@@ -1,11 +1,28 @@
 "use client"
 
+import { z } from 'zod';
+import { createChaptersSchema } from '@/validators/course';
+import { Form } from './ui/form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 interface CreateCourseFormProps { }
 
+type Input = z.infer<typeof createChaptersSchema>;
+
 export const CreateCourseForm = () => {
+  const form = useForm<Input>({
+    resolver: zodResolver(createChaptersSchema),
+    defaultValues: {
+      title
+    }
+  })
+
   return (
     <div className='w-full'>
-
+      <Form>
+        <form></form>
+      </Form>
     </div>
   );
 }
