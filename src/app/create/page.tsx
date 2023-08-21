467befb5f671e13be9bf8dc63e3aa8edcfab9384
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 06:27:00 +0100

    Created info

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,5 @@
 import { getAuthSession } from '@/lib/auth';
+import { InfoIcon } from 'lucide-react';
 import { redirect } from 'next/navigation';
 
 interface CreatePageProps { }
@@ -12,7 +13,11 @@ const CreatePage = async () => {
   return (
 
     <div className='flex flex-col items-center max-w-xl px-8 mx-auto my-16 sm:px-0'>
-      <h1 className='self-center text-3xl font-bold text-center sm:text-6xl'>Learnify</h1>
+      <h1 className='self-center text-3xl font-bold text-center sm:text-6xl'>Learning Jurney</h1>
+      <div className='flex p-4 mt-5 border-none bg-secondary'>
+        <InfoIcon className='w-12 h-12 mr-3 text-blue-400' />
+        <div>Enter in a course title, or what you want to learn about. Then enter a list of units which are the specifics you want to learn. And our AI will generate a course for your</div>
+      </div>
     </div>
   );
 }
