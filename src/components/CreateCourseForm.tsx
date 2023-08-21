5a5a969f91092a2359a21f22db06ecbe25194783
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 07:16:17 +0100

    Added framer motion and wrapped form

--- src/components/CreateCourseForm.tsx
+++ src/components/CreateCourseForm.tsx
@@ -9,6 +9,7 @@ import { Input } from './ui/input';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
 import { Plus, Trash, Wand2 } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface CreateCourseFormProps { }
 
@@ -33,6 +34,8 @@ export const CreateCourseForm = () => {
     form.setValue("units", filteredUnits);
   };
 
+  form.watch();
+
   return (
     <div className='w-full'>
       <Form {...form}>
@@ -54,30 +57,39 @@ export const CreateCourseForm = () => {
               );
             }}
           />
-          {form.watch("units").map((_, index) => {
-            return (
-              <FormField
-                key={index}
-                control={form.control}
-                name={`units.${index}`}
-                render={({ field }) => {
-                  return (
-                    <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
-                      <FormLabel className="flex-[1] text-xl">
-                        Unit {index + 1}
-                      </FormLabel>
-                      <FormControl className="flex-[6]">
-                        <Input
-                          placeholder="Enter subtopic of the course"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  );
-                }}
-              />
-            )
-          })}
+          <AnimatePresence>
+            {form.watch("units").map((_, index) => {
+              return (
+                <motion.div key={index}
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: "auto" }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{ opacity: { duration: 0.2 }, height: { duration: 0.2 } }}
+                >
+                  <FormField
+                    key={index}
+                    control={form.control}
+                    name={`units.${index}`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                          <FormLabel className="flex-[1] text-xl">
+                            Unit {index + 1}
+                          </FormLabel>
+                          <FormControl className="flex-[6]">
+                            <Input
+                              placeholder="Enter subtopic of the course"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </motion.div>
+              )
+            })}
+          </AnimatePresence>
           <div className="flex items-center justify-center mt-4">
             <Separator className="flex-[1]" />
             <div className="mx-4">
@@ -118,6 +130,6 @@ export const CreateCourseForm = () => {
           </div>
         </form>
       </Form>
-    </div>
+    </div >
   );
 }
