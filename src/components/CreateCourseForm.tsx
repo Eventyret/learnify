9cfd5593edea0427f9cafcf9b8f5c0c7714450b3
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 06:41:45 +0100

    Added title for form

--- src/components/CreateCourseForm.tsx
+++ src/components/CreateCourseForm.tsx
@@ -2,9 +2,10 @@
 
 import { z } from 'zod';
 import { createChaptersSchema } from '@/validators/course';
-import { Form } from './ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from './ui/input';
 
 interface CreateCourseFormProps { }
 
@@ -26,8 +27,24 @@ export const CreateCourseForm = () => {
   return (
     <div className='w-full'>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-
+        <form onSubmit={form.handleSubmit(onSubmit)} className='w-full mt-4'>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => {
+              return (
+                <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                  <FormLabel className="flex-[1] text-xl">Title</FormLabel>
+                  <FormControl className="flex-[6]">
+                    <Input
+                      placeholder="Enter the main topic of the course"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
         </form>
       </Form>
     </div>
