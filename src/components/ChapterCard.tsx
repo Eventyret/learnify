a759c58e1294ce3895cc08e2a79ef5ba732cb17e
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 11:16:45 +0100

    Working on ChapterCard UI

--- src/components/ChapterCard.tsx
+++ src/components/ChapterCard.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import axios from 'axios';
 import { cn } from '@/lib/utils';
 import { Chapter } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 
 interface ChapterCardProps {
@@ -11,6 +13,12 @@ interface ChapterCardProps {
 
 const ChapterCard: React.FC<ChapterCardProps> = ({ chapter, chapterIndex }) => {
   const [success, setSuccess] = useState<boolean | null>(null)
+  const { mutate: getChapterInfo, isLoading } = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post('/api/chapter/getInfo')
+      return response.data
+    }
+  })
   return (
     <div key={chapter.id} className={cn("px-4 py-2 mt-2 rounded flex justify-between", {
       "bg-secondary": success === null,
