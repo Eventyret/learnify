2b7e74bafa246570cabd5f32e779875e057786d5
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-21 15:30:18 +0100

    Working on getting chapters

--- src/app/api/chapter/getInfo/route.ts
+++ src/app/api/chapter/getInfo/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from '@/lib/db'
 import { NextResponse } from 'next/server'
 import { z } from 'zod'
 
@@ -9,6 +10,18 @@ export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json()
     const { chapterId } = bodyParser.parse(body)
+
+    const chapter = await prisma.chapter.findUnique({
+      where: {
+        id: chapterId
+      }
+    })
+    if (!chapter) {
+      return NextResponse.json({
+        sucess: false,
+        error: "chapter not found"
+      }, { status: 404 })
+    }
     return NextResponse.json({ hello: "world" })
   } catch (error) {
     if (error instanceof z.ZodError) {
