83fe60e2149c19b76a62542550f341465cedabe7
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-23 07:56:26 +0100

    Working on design for gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+
+interface GalleryPageProps { }
+
+const GalleryPage: React.FC<GalleryPageProps> = async () => {
+  return (
+    <div>
+      GalleryPage
+    </div>
+  );
+}
+export default GalleryPage;
